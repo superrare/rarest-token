8c5abd3c3273de70520d7b519d16893f431830f9
Zach Kolodny <kolodnyz@icloud.com>
2021-05-18 13:41:06 -0400

    updated deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,15 +10,10 @@ async function main() {
 
   const Token = await ethers.getContractFactory("SuperRareToken");
 
-  const proxy = await upgrades.deployProxy(Token, ['0xd5a498bbc6d21e4e1cdbb8fec58e3ecd7124fb43'], {initializer: 'init'});
-  // const ProxyContractFactory = await ethers.getContractFactory("SuperRareTokenProxy");
+  const proxy = await upgrades.deployProxy(Token, [deployer.address], {initializer: 'init'});
   
-  // const token = await ethers.
-
-
   await proxy.deployed();
 
-  // console.log("Token address:", token.address);
   console.log("Token address:", proxy.address);
 };
 
