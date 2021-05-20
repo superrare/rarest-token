8f4ef067142dbc6bdff7b23a94f3c4603c7cd366
Andrew Tam <tam.andrew7@gmail.com>
2021-05-20 10:31:15 -0700

    Add pausing & liquidity checks

--- contracts/staking/SuperRareStaking.sol
+++ contracts/staking/SuperRareStaking.sol
@@ -4,24 +4,26 @@ pragma solidity ^0.7.3;
 
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/PausableUpgradeable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ISuperRareStaking.sol";
 
-contract SuperRareStaking is ISuperRareStaking, Initializable, OwnableUpgradeable {
+contract SuperRareStaking is ISuperRareStaking, Initializable, OwnableUpgradeable, PausableUpgradeable {
   struct Stake {
       uint256 amount;
       uint256 length;
-	  uint256 startingTime;
+      uint256 startingTime;
+      uint256 reward;
   }
 
   IERC20 stakingToken;
   uint256 totalStaked;
+  uint256 totalPendingRewards;
   address poolAddress;
 
   mapping (address => uint256) totalStakeBalances;
   mapping (address => Stake[]) stakes;
 
-  uint256[] public stakingLengths;  // Is this necessary?
   mapping (uint256 => uint256) public rewardRatios;
 
   function initialize(
@@ -35,25 +37,31 @@ contract SuperRareStaking is ISuperRareStaking, Initializable, OwnableUpgradeabl
       for (uint256 i = 0; i < _durations.length; i++) {
         rewardRatios[_durations[i]] = _rates[i];
       }
-      stakingLengths = _durations;
       stakingToken = IERC20(_tokenAddress);
       poolAddress = _poolAddress;
   }
 
-  function stake(uint256 amount, uint256 length) external override {
+  /* MUTABLE */
+  function stake(uint256 amount, uint256 length) external override whenNotPaused {
       require (amount > 0, "Must stake more than 0 tokens."); // Might change this to have some sort of minimum
       require (stakingToken.balanceOf(msg.sender) >= amount, "User does not have enough token.");
       require (rewardRatios[length] > 0, "Invalid length."); // Check that it is a valid length
 
+      uint256 reward = amount * rewardRatios[length]/100;
+      require (stakingToken.balanceOf(poolAddress) >= totalStaked + totalPendingRewards + reward,
+        "Pool does not have enough liquidity.");
+
       Stake memory newStake = Stake (
           amount,
           length,
-          block.timestamp
+          block.timestamp,
+          reward
       );
 
       stakes[msg.sender].push(newStake);
       totalStakeBalances[msg.sender] += amount;
       totalStaked += amount;
+      totalPendingRewards += reward;
 
       stakingToken.transferFrom(msg.sender, poolAddress, amount);
       emit Staked(msg.sender, stakes[msg.sender].length, amount, length);
@@ -64,24 +72,25 @@ contract SuperRareStaking is ISuperRareStaking, Initializable, OwnableUpgradeabl
       require (currentStake.amount > 0, "Invalid stake index or no stake.");
       require (block.timestamp >= currentStake.startingTime + currentStake.length, "Stake has not expired yet.");
 
-      // Update state variables
       uint256 amount = currentStake.amount;
       uint256 length = currentStake.length;
-      totalStaked -= amount;
-      totalStakeBalances[msg.sender] -= amount;
+      uint256 reward = currentStake.reward;
 
       // Delete stake entry
       delete stakes[msg.sender][index];
 
-      // Calculate and transfer reward
-      uint256 reward = amount * rewardRatios[length]/100;
-      /* Add transfer from pool here. */
+      // Update state variables
+      totalStakeBalances[msg.sender] -= amount;
+      totalStaked -= amount;
+      totalPendingRewards -= reward;
 
-      stakingToken.transferFrom(poolAddress, msg.sender, amount);
+      // Transfer original stake + reward
+      stakingToken.transferFrom(poolAddress, msg.sender, amount+reward);
 
       emit Unstaked(msg.sender, index, amount, length);
   }
 
+  /* GETTERS */
   function getTotalStaked() external override view returns (uint256) {
       return totalStaked;
   }
@@ -94,7 +103,29 @@ contract SuperRareStaking is ISuperRareStaking, Initializable, OwnableUpgradeabl
     return totalStakeBalances[staker];
   }
 
+  function getPoolAddress() external view returns (address) {
+      return poolAddress;
+  }
+
+  /* ADMIN FUNCTIONS */
   function updateRewardRatio(uint256 length, uint256 rate) external onlyOwner {
       rewardRatios[length] = rate;
+  }
+
+  function setPaused(bool _paused) external onlyOwner {
+      if (_paused == paused()) {
+          return;
+      }
+
+      if (_paused) {
+          _pause();
+      }
+      else {
+          _unpause();
+      }
+  }
+
+  function setPoolAddress(address _poolAddress) external onlyOwner {
+      poolAddress = _poolAddress;
   }
 }
