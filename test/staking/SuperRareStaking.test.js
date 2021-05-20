b980ea979a2c1af331a5184751de84e20b6b97e3
Zach Kolodny <kolodnyz@icloud.com>
2021-05-20 14:52:13 -0400

    Added unstaking tests

--- test/staking/SuperRareStaking.test.js
+++ test/staking/SuperRareStaking.test.js
@@ -38,7 +38,6 @@ describe('SuperRareStaking', function () {
       // Arrange
       const srTokenAddress = this.superRareToken.address;
       const rewards = [2, 5, 10];
-      const durations = [2592000, 7776000, 15552000];
       
       // Act
       const stakingTokenAddress = await this.superRareStaking.token();
@@ -52,6 +51,10 @@ describe('SuperRareStaking', function () {
       expect(ninetyDayRate).to.equal(rewards[1]);
       expect(oneEightyDayRate).to.equal(rewards[2]);
     });
+  });
+
+  describe('Admin Functions', function () {
+
   });
 
   describe('Stake', function () {
@@ -154,7 +157,7 @@ describe('SuperRareStaking', function () {
       expect(totalStaked).to.equal("20000000000000000000");
     });
 
-    it('Pool Should Have Enough To Payout Reward', async function() {
+    it('Pool Doesnt Have Enough Liquidity', async function() {
       // Arrange 
       const [owner] = await ethers.getSigners();
       const durations = [2592000, 7776000, 15552000];
@@ -171,6 +174,170 @@ describe('SuperRareStaking', function () {
       // Assert
       expect(amtStakedByUser).to.equal("0");
       expect(totalStaked).to.equal("0");
+    });
+  });
+
+  describe('Unstake', async function () {
+    it('Single User - success', async function () {
+      // Arrange 
+      const [_, addr1] = await ethers.getSigners();
+      const durations = [2592000, 7776000, 15552000];
+      await this.superRareToken.transfer(this.superRareStaking.address, "100000000000000000000000");
+      await this.superRareToken.transfer(addr1.address, "1000");
+      await this.superRareToken.connect(addr1).approve(this.superRareStaking.address, "1000000000000000000000000");
+      await this.superRareStaking.connect(addr1).stake("1000", durations[0]);
+  
+      // Act
+      const amtStakedByUserBefore = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedBefore = (await this.superRareStaking.getTotalStaked()).toString();
+      await advanceTime(durations[0]);
+      await this.superRareStaking.connect(addr1).unstake(0);
+      const amtStakedByUserAfter = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedAfter = (await this.superRareStaking.getTotalStaked()).toString();
+      const balanceOfUser = (await this.superRareToken.balanceOf(addr1.address)).toString();
+  
+      // Assert
+      expect(amtStakedByUserBefore).to.equal("1000");
+      expect(totalStakedBefore).to.equal("1000");
+      expect(amtStakedByUserAfter).to.equal("0");
+      expect(totalStakedAfter).to.equal("0");
+      expect(balanceOfUser).to.equal("1020");
+    });
+
+    it('Single User - Time Not Up', async function () {
+      // Arrange 
+      const [_, addr1] = await ethers.getSigners();
+      const durations = [2592000, 7776000, 15552000];
+      await this.superRareToken.transfer(this.superRareStaking.address, "100000000000000000000000");
+      await this.superRareToken.transfer(addr1.address, "1000");
+      await this.superRareToken.connect(addr1).approve(this.superRareStaking.address, "1000000000000000000000000");
+      await this.superRareStaking.connect(addr1).stake("1000", durations[0]);
+  
+      // Act
+      const amtStakedByUserBefore = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedBefore = (await this.superRareStaking.getTotalStaked()).toString();
+      await expectRevert(
+        this.superRareStaking.connect(addr1).unstake(0),
+        "Stake has not expired yet."
+      );
+      const amtStakedByUserAfter = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedAfter = (await this.superRareStaking.getTotalStaked()).toString();
+      const balanceOfUser = (await this.superRareToken.balanceOf(addr1.address)).toString();
+  
+      // Assert
+      expect(amtStakedByUserBefore).to.equal("1000");
+      expect(totalStakedBefore).to.equal("1000");
+      expect(amtStakedByUserAfter).to.equal("1000");
+      expect(totalStakedAfter).to.equal("1000");
+      expect(balanceOfUser).to.equal("0");
+    });
+
+    it('Single User - Cant Unstake If Not Staked', async function () {
+      // Arrange 
+      const [_, addr1] = await ethers.getSigners();
+      await this.superRareToken.transfer(this.superRareStaking.address, "100000000000000000000000");
+      await this.superRareToken.transfer(addr1.address, "1000");
+      await this.superRareToken.connect(addr1).approve(this.superRareStaking.address, "1000000000000000000000000");
+  
+      // Act
+      const amtStakedByUserBefore = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedBefore = (await this.superRareStaking.getTotalStaked()).toString();
+      await expectRevert(
+        this.superRareStaking.connect(addr1).unstake(0),
+        "Invalid stake index or no stake."
+      );
+      const amtStakedByUserAfter = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedAfter = (await this.superRareStaking.getTotalStaked()).toString();
+      const balanceOfUser = (await this.superRareToken.balanceOf(addr1.address)).toString();
+  
+      // Assert
+      expect(amtStakedByUserBefore).to.equal("0");
+      expect(totalStakedBefore).to.equal("0");
+      expect(amtStakedByUserAfter).to.equal("0");
+      expect(totalStakedAfter).to.equal("0");
+      expect(balanceOfUser).to.equal("0");
+    });
+
+    it('Single User - Cant Unstake NonExistent Stake', async function () {
+      // Arrange 
+      const [_, addr1] = await ethers.getSigners();
+      const durations = [2592000, 7776000, 15552000];
+      await this.superRareToken.transfer(this.superRareStaking.address, "100000000000000000000000");
+      await this.superRareToken.transfer(addr1.address, "1000");
+      await this.superRareToken.connect(addr1).approve(this.superRareStaking.address, "1000000000000000000000000");
+      await this.superRareStaking.connect(addr1).stake("1000", durations[0]);
+  
+      // Act
+      const amtStakedByUserBefore = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedBefore = (await this.superRareStaking.getTotalStaked()).toString();
+      await expectRevert(
+        this.superRareStaking.connect(addr1).unstake(1),
+        "Invalid stake index or no stake."
+      );
+      const amtStakedByUserAfter = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const totalStakedAfter = (await this.superRareStaking.getTotalStaked()).toString();
+      const balanceOfUser = (await this.superRareToken.balanceOf(addr1.address)).toString();
+  
+      // Assert
+      expect(amtStakedByUserBefore).to.equal("0");
+      expect(totalStakedBefore).to.equal("0");
+      expect(amtStakedByUserAfter).to.equal("0");
+      expect(totalStakedAfter).to.equal("0");
+      expect(balanceOfUser).to.equal("0");
+    });
+
+    it('Multi User - success', async function () {
+      // Arrange 
+      const [_, addr1, addr2] = await ethers.getSigners();
+      const durations = [2592000, 7776000, 15552000];
+      await this.superRareToken.transfer(this.superRareStaking.address, "100000000000000000000000");
+      await this.superRareToken.transfer(addr1.address, "1000");
+      await this.superRareToken.transfer(addr2.address, "2000");
+      await this.superRareToken.connect(addr1).approve(this.superRareStaking.address, "1000000000000000000000000");
+      await this.superRareToken.connect(addr2).approve(this.superRareStaking.address, "1000000000000000000000000");
+      await this.superRareStaking.connect(addr1).stake("1000", durations[0]);
+      await this.superRareStaking.connect(addr2).stake("2000", durations[1]);
+      const timeDiff = durations[1] - durations[0];
+  
+      // Act
+      const amtStakedByUser1Before = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const amtStakedByUser2Before = (await this.superRareStaking.getTotalStakedByAddress(addr2.address)).toString();
+      const totalStakedBefore = (await this.superRareStaking.getTotalStaked()).toString();
+      
+      await advanceTime(durations[0]);
+      await this.superRareStaking.connect(addr1).unstake(0);
+
+      const amtStakedByUser1Mid = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const amtStakedByUser2Mid = (await this.superRareStaking.getTotalStakedByAddress(addr2.address)).toString();
+      const totalStakedMid = (await this.superRareStaking.getTotalStaked()).toString();
+      const balanceOfUser1Mid = (await this.superRareToken.balanceOf(addr1.address)).toString();
+      const balanceOfUser2Mid = (await this.superRareToken.balanceOf(addr2.address)).toString();
+
+      await advanceTime(timeDiff);
+      await this.superRareStaking.connect(addr2).unstake(0);
+
+      const amtStakedByUser1After = (await this.superRareStaking.getTotalStakedByAddress(addr1.address)).toString();
+      const amtStakedByUser2After = (await this.superRareStaking.getTotalStakedByAddress(addr2.address)).toString();
+      const totalStakedAfter = (await this.superRareStaking.getTotalStaked()).toString();
+      const balanceOfUser1After = (await this.superRareToken.balanceOf(addr1.address)).toString();
+      const balanceOfUser2After = (await this.superRareToken.balanceOf(addr2.address)).toString();
+  
+      // Assert
+      expect(amtStakedByUser1Before).to.equal("1000");
+      expect(amtStakedByUser2Before).to.equal("2000");
+      expect(totalStakedBefore).to.equal("3000");
+      
+      expect(amtStakedByUser1Mid).to.equal("0");
+      expect(amtStakedByUser2Mid).to.equal("2000");
+      expect(totalStakedMid).to.equal("2000");
+      expect(balanceOfUser1Mid).to.equal("1020");
+      expect(balanceOfUser2Mid).to.equal("0");
+
+      expect(amtStakedByUser1After).to.equal("0");
+      expect(amtStakedByUser2After).to.equal("0");
+      expect(totalStakedAfter).to.equal("0");
+      expect(balanceOfUser1After).to.equal("1020");
+      expect(balanceOfUser2After).to.equal("2100");
     });
   });
 });
