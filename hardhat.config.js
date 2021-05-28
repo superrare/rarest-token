a7ce7d598ba9988f29ea3553c2a64d3e268cd9e3
Zach Kolodny <kolodnyz@icloud.com>
2021-05-28 13:13:40 -0400

    updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,10 +2,6 @@
 require("@nomiclabs/hardhat-ethers");
 require('@openzeppelin/hardhat-upgrades');
 require("@nomiclabs/hardhat-waffle");
-<<<<<<< HEAD
-=======
-//const { ropstenNodeUrl, rinkebyNodeUrl, privateKey } = require('./secrets.json');
->>>>>>> 9f6f923 (Start implementation)
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -13,16 +9,5 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.7.3",
   networks: {
-<<<<<<< HEAD
-=======
-    // ropsten: {
-    //   url: ropstenNodeUrl,
-    //   accounts: [`0x${privateKey}`]
-    // },
-    // rinkeby: {
-    //   url: rinkebyNodeUrl,
-    //   accounts: [`0x${privateKey}`]
-    // }
->>>>>>> 9f6f923 (Start implementation)
   }
 };
